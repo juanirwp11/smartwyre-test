7ddd03a7c66f21f07dd956b67bb3f91ce8293290
juanirwp11 <juanirwp@hotmail.com>
2024-07-26 13:14:11 +0200

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,7 @@
 provider "azurerm" {
   features {}
+  client_id       = "ad73cf00-da95-4fd4-8183-f00a635c1a30"
+  client_secret   = "smF8Q~fUDf6BakdVqlsKvMgY_bY1HBAxI5qq3bQo"
+  tenant_id       = "3abc5e4e-1080-4991-a58f-815a337e150b"
+  subscription_id = "41baa1e3-240b-4ac6-9767-ebff98622ea2"
 }
