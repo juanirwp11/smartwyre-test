6f44fe3157fbd21ddac5b6cb277b9654878e92ad
juanirwp11 <juanirwp@hotmail.com>
2024-07-26 16:15:13 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,22 @@
 module "network" {
   source = "./modules/network"
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
 
 module "acr" {
   source = "./modules/acr"
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
 
 module "aks" {
   source = "./modules/aks"
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
 
 output "kubeconfig" {
-  value = module.aks.kubeconfig
+  value     = module.aks.kubeconfig
   sensitive = true
 }
