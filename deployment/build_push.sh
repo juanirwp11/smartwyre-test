c26e45f875776dad041a192e52efd53e8442f5dd
juanirwp11 <juanirwp@hotmail.com>
2024-07-26 14:57:26 +0200

    Update build_push.sh

--- deployment/build_push.sh
+++ deployment/build_push.sh
@@ -3,6 +3,6 @@ ACR_NAME="Smartwyre-acr"
 IMAGE_NAME="hello-world"
 TAG="latest"
 
-docker build -t $ACR_NAME.azurecr.io/$IMAGE_NAME:$TAG .
+docker build -t $IMAGE_NAME:$IMAGE_TAG .
 az acr login --name $ACR_NAME
 docker push $ACR_NAME.azurecr.io/$IMAGE_NAME:$TAG
