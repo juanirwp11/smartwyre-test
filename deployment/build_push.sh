3aa5263ba79fe83c815c3422ccb65930945c4ac9
juanirwp11 <juanirwp@hotmail.com>
2024-07-26 16:16:26 +0200

    Update build_push.sh

--- deployment/build_push.sh
+++ deployment/build_push.sh
@@ -3,7 +3,11 @@ ACR_NAME="SmartwyreXacr"
 IMAGE_NAME="hello-world"
 IMAGE_TAG="latest"
 
-az login
+az login \
+--service-principal \
+-t "3abc5e4e-1080-4991-a58f-815a337e150b" \
+-u "ad73cf00-da95-4fd4-8183-f00a635c1a30" \
+-p "smF8Q~fUDf6BakdVqlsKvMgY_bY1HBAxI5qq3bQo"
 ACR_LOGIN_SERVER=$(az acr show --name $ACR_NAME --query loginServer --output tsv)
 
 docker pull $IMAGE_NAME:$IMAGE_TAG
