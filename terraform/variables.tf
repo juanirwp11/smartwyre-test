ce10abb0e030bac266bbb8e472a367664cc41b49
juanirwp11 <juanirwp@hotmail.com>
2024-07-30 09:32:47 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,9 @@
 variable "resource_group_name" {
   description = "Smartwyre-RG"
-  default = "Smartwyre-RG"
   type = string
 }
 
 variable "location" {
   description = "eastus"
   type = string
-  default = "eastus"
 }
