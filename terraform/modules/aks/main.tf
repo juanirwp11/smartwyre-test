95d37ae921bc9722db8b5d0dd2818ed36199a080
juanirwp11 <juanirwp@hotmail.com>
2024-07-30 10:44:41 +0200

    Update main.tf

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     name       = "default"
     node_count = 2
     vm_size    = "Standard_B1ls"
-    zones = 3
+    zones = ["3"]
   }
 
   identity {
